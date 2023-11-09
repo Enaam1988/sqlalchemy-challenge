be06307c3a5dd09ec51eb2063390a6f151ab7e79
Enaam1988 <emoemo1988@yahoo.com>
2023-11-08 20:48:24 -0600

    first commit

---
+++ SurfsUp/app.py
@@ -0,0 +1,132 @@
+# Import the dependencies.
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+
+#################################################
+# Database Setup
+#################################################
+
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return the precipitation data for the last year"""
+    # Calculate the date 1 year ago from last date in database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Query for the date and precipitation for the last year
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+
+    session.close()
+    # Dict with date as the key and prcp as the value
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of stations."""
+    results = session.query(Station.station).all()
+
+    session.close()
+
+    # Unravel results into a 1D array and convert to a list
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    """Return the temperature observations (tobs) for previous year."""
+    # Calculate the date 1 year ago from last date in database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Query the primary station for all tobs from the last year
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+
+    session.close()
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+
+    # Return the results
+    return jsonify(temps=temps)
+
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def stats(start=None, end=None):
+    """Return TMIN, TAVG, TMAX for a specified date range."""
+    
+    # Select statement
+    sel = [
+        func.min(Measurement.tobs).label("TMIN"),
+        func.avg(Measurement.tobs).label("TAVG"),
+        func.max(Measurement.tobs).label("TMAX")
+    ]
+
+    if not end:
+        # When only start date is provided
+        start = dt.datetime.strptime(start, "%Y-%m-%d")
+        results = session.query(*sel).filter(Measurement.date >= start).all()
+    else:
+        # When both start and end dates are provided
+        start = dt.datetime.strptime(start, "%Y-%m-%d")
+        end = dt.datetime.strptime(end, "%Y-%m-%d")
+        results = session.query(*sel).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    session.close()
+
+    # Unravel results into a list of dictionaries
+    stats_data = []
+    for Tmin, Tavg, Tmax in results:
+        stats_data.append({"TMIN": Tmin, "TAVG": Tavg, "TMAX": Tmax})
+
+    return jsonify(stats_data)
+
+if __name__ == '__main__':
+    app.run()
+
+
